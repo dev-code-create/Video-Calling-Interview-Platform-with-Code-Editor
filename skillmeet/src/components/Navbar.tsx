d5acffbbd75966929f7000f895cc932c0f31a804
dev-code-create <ayushkankale04@gmail.com>
2025-01-30 23:41:09 +0530

    Navbar done : )

--- skillmeet/src/components/Navbar.tsx
+++ skillmeet/src/components/Navbar.tsx
@@ -1,14 +1,31 @@
 import Link from "next/link";
 import { ModeToggle } from "./ModeToggle";
 import { CodeIcon } from "lucide-react";
+import { SignedIn, UserButton } from "@clerk/nextjs";
+import DashboardBtn from "./DashboardBtn";
 
 const Navbar = () => {
   return (
     <nav>
-      <div>
-        <Link href="/">
-          <CodeIcon />
+      <div className="flex h-16 items-center px-4 container mx-auto">
+        {/* Left Side Logo */}
+        <Link
+          href="/"
+          className="flex items-center gap-2 font-semibold text-2xl mr-6 font-mono hover:opacity-80"
+        >
+          <CodeIcon className="size-8 text-emerald-500" />
+          <span className="bg-gradient-to-r from-emerald-600 to-teal-500 bg-clip-text text-transparent">
+            SkillMeet
+          </span>
         </Link>
+        {/* Right side */}
+        <SignedIn>
+          <div className="flex items-center space-x-4 ml-auto">
+            <DashboardBtn />
+            <ModeToggle />
+            <UserButton />
+          </div>
+        </SignedIn>
       </div>
     </nav>
   );
