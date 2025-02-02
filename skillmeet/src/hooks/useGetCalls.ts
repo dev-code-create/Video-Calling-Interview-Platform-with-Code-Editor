a886e87d15174fafb0c6e5089cbd2b9556054f41
dev-code-create <ayushkankale04@gmail.com>
2025-02-02 17:02:27 +0530

    Recordings Page Done

--- skillmeet/src/hooks/useGetCalls.ts
+++ skillmeet/src/hooks/useGetCalls.ts
@@ -1,34 +1,57 @@
 import { useEffect, useState } from "react";
+import { useUser } from "@clerk/nextjs";
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 
-const useGetCallById = (id: string | string[]) => {
-  const [call, setCall] = useState<Call>();
-  const [isCallLoading, setIsCallLoading] = useState(true);
-
+const useGetCalls = () => {
+  const { user } = useUser();
   const client = useStreamVideoClient();
+  const [calls, setCalls] = useState<Call[]>();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    if (!client) return;
+    const loadCalls = async () => {
+      if (!client || !user?.id) return;
 
-    const getCall = async () => {
+      setIsLoading(true);
+
       try {
         const { calls } = await client.queryCalls({
-          filter_conditions: { id },
+          sort: [{ field: "starts_at", direction: -1 }],
+          filter_conditions: {
+            starts_at: { $exists: true },
+            $or: [
+              { created_by_user_id: user.id },
+              { members: { $in: [user.id] } },
+            ],
+          },
         });
 
-        if (calls.length > 0) setCall(calls[0]);
+        setCalls(calls);
       } catch (error) {
         console.error(error);
-        setCall(undefined);
       } finally {
-        setIsCallLoading(false);
+        setIsLoading(false);
       }
     };
 
-    getCall();
-  }, [client, id]);
+    loadCalls();
+  }, [client, user?.id]);
 
-  return { call, isCallLoading };
+  const now = new Date();
+
+  const endedCalls = calls?.filter(({ state: { startsAt, endedAt } }: Call) => {
+    return (startsAt && new Date(startsAt) < now) || !!endedAt;
+  });
+
+  const upcomingCalls = calls?.filter(({ state: { startsAt } }: Call) => {
+    return startsAt && new Date(startsAt) > now;
+  });
+
+  const liveCalls = calls?.filter(({ state: { startsAt, endedAt } }: Call) => {
+    return startsAt && new Date(startsAt) < now && !endedAt;
+  });
+
+  return { calls, endedCalls, upcomingCalls, liveCalls, isLoading };
 };
 
-export default useGetCallById;
+export default useGetCalls;
